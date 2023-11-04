83dd89dab28f3514e467432a65c4ab3bcde868bb
chiha <chihab.kimo@gmail.com>
2023-11-04 18:57:41 +0100

    added few touches

--- app/src/main/java/com/example/app/SignUpActivity.java
+++ app/src/main/java/com/example/app/SignUpActivity.java
@@ -2,13 +2,23 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+import com.example.app.R;
+
 
 public class SignUpActivity extends AppCompatActivity {
-
+    private TextView goToSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        goToSignIn = findViewById(R.id.goToSignIn);
+
+        goToSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(SignUpActivity.this,SignInActivity.class));
+        });
     }
 }
