220cb3fd3747d4716f4c82448acb948e3169ab31
chiha <chihab.kimo@gmail.com>
2023-11-09 09:40:32 +0100

    added few touches

--- app/src/main/java/com/example/app/RecoverActivity.java
+++ app/src/main/java/com/example/app/RecoverActivity.java
@@ -2,13 +2,34 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RecoverActivity extends AppCompatActivity {
+
+    private Button changePass;
+    private Button backToSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recover);
+
+        changePass = findViewById(R.id.changePass);
+        backToSignIn = findViewById(R.id.backToSignIn);
+
+        backToSignIn.setOnClickListener(v -> {
+            startActivity(new Intent(RecoverActivity.this, SignInActivity.class));
+        });
+
+        changePass.setOnClickListener(v -> {
+            showToast("Password changed successfully!");
+        });
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/app/SignInActivity.java
+++ app/src/main/java/com/example/app/SignInActivity.java
@@ -2,13 +2,36 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private TextView forgetPass;
+    private TextView goToSignUp;
+    private Button signIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        forgetPass = findViewById(R.id.forgetPass);
+        goToSignUp = findViewById(R.id.goToSignUp);
+        signIn = findViewById(R.id.signIn);
+
+        goToSignUp.setOnClickListener(v -> {
+            startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
+        });
+
+        forgetPass.setOnClickListener(v -> {
+            startActivity((new Intent(SignInActivity.this,RecoverActivity.class)));
+        });
+
+        signIn.setOnClickListener(v -> {
+            startActivity(new Intent(SignInActivity.this, HomeScrenn.class));
+        });
     }
 }
